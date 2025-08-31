d92b45cfdf6445721db5bd8d5f7ace12883dc951
macbucheron1 <nathandeprat@hotmail.fr>
2025-08-31 21:45:08 +0200

    feat: addind declarative firefox extensions

--- home/firefox/default.nix
+++ home/firefox/default.nix
@@ -1,4 +1,4 @@
-{ inputs, ... }:
+{ ... }:
 {
   programs.firefox = {
     enable = true;
@@ -30,6 +30,28 @@
             ];
           }
         ];
+      };
+    };
+
+    policies = {
+      ExtensionSettings = {
+        # Dark Reader
+        "addon@darkreader.org" = {
+          installation_mode = "force_installed";
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/darkreader/latest.xpi";
+        };
+
+        # uBlock Origin
+        "uBlock0@raymondhill.net" = {
+          installation_mode = "force_installed";
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+        };
+
+        # Bitwarden
+        "446900e4-71c2-419f-a6a7-df9c091e268b" = {
+          installation_mode = "force_installed";
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
+        };
       };
     };
   };
