6887e2af372ebb4e9d4ef022b7ba5fe4f25c20ae
macbucheron1 <nathandeprat@hotmail.fr>
2025-10-02 09:29:57 +0200

    feat: updating all input

--- home/git/default.nix
+++ home/git/default.nix
@@ -3,11 +3,18 @@
   programs.git = {
     enable = true;
     package = pkgs.gitFull;
+
     userName  = "macbucheron1";
     userEmail = "nathandeprat@hotmail.fr";
+
     extraConfig = {
       init.defaultBranch = "main";
       pull.rebase = true;
+
+      commit.gpgsign = true;
+
+      gpg.format = "ssh";
+      user.signingkey = "~/.ssh/id_ed25519";
     };
   };
 }
