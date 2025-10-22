ddb07917ef8147b097dffd37e62df1a637438ef8
macbucheron1 <nathandeprat@hotmail.fr>
2025-10-22 13:55:07 +0200

    fix: removing vbox

--- home/default.nix
+++ home/default.nix
@@ -16,9 +16,6 @@
     nautilus # Explorateur de fichiers GNOME
     obsidian #https://discordapp.com/channels/568306982717751326/1283818433049530380/1383786696038027264
     libreoffice
-
-    virtualbox
-
   ];
 
   xdg.userDirs = {
