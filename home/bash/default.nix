81cae823c37d2bd3533db14366914a444caf9633
macbucheron1 <nathandeprat@hotmail.fr>
2026-01-08 13:49:15 +0100

    feat: prompt

--- home/bash/default.nix
+++ home/bash/default.nix
@@ -1,6 +1,6 @@
-{ ... }:
+{ config, ... }:
 {
-# Change the cursor into a barre for insert mode / bloc in normal mode
+  # Change the cursor into a barre for insert mode / bloc in normal mode
   programs.readline = {
     enable = true;
     includeSystemConfig = true;
@@ -12,16 +12,45 @@
     };
   };
 
-
   programs.bash = {
     enable = true;
     enableCompletion = true;
+
     bashrcExtra = ''
       set -o vi
+
+      # --- PS1 stylé, couleurs depuis Stylix ---
+      # On récupère les couleurs Stylix (hex avec '#')
+      _sx_icon="${config.lib.stylix.colors.withHashtag.base0D}"
+      _sx_user="${config.lib.stylix.colors.withHashtag.base0B}"
+      _sx_path="${config.lib.stylix.colors.withHashtag.base0C}"
+
+      _sx_hex_to_rgb() {
+        local hex="''${1#\#}"
+        printf "%d;%d;%d" \
+          $((16#''${hex:0:2})) \
+          $((16#''${hex:2:2})) \
+          $((16#''${hex:4:2}))
+      }
+
+      _sx_fg() {
+        printf '\[\e[38;2;%sm\]' "$(_sx_hex_to_rgb "$1")"
+      }
+
+      _sx_reset='\[\e[0m\]'
+      _sx_bold='\[\e[1m\]'
+
+      _c_icon="$(_sx_fg "$_sx_icon")"
+      _c_user="$(_sx_fg "$_sx_user")"
+      _c_path="$(_sx_fg "$_sx_path")"
+
+      export PS1="''${_c_icon}''${_sx_reset} ''${_sx_bold}''${_c_user}\u ''${_c_path}\w''${_sx_reset} ''${_sx_bold}\$''${_sx_reset} "
     '';
+
     initExtra = ''
       nitch
     '';
+
     shellAliases = { c = "clear"; };
   };
 }
